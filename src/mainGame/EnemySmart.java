f5e116a785d098d8d3bd9493b34948457d4ddea7
brody10703 <32171473+brody10703@users.noreply.github.com>
2017-11-09 15:00:51 -0500

    Added Boundary and Removal

--- src/mainGame/EnemySmart.java
+++ src/mainGame/EnemySmart.java
@@ -74,7 +74,10 @@ public class EnemySmart extends GameObject {
 		// if (this.y <= 0 || this.y >= Game.HEIGHT - 40) velY *= -1;
 		// if (this.x <= 0 || this.x >= Game.WIDTH - 16) velX *= -1;
 
-	
+		if (this.y >= Game.HEIGHT * 2 || this.y <= Game.HEIGHT *-2 || this.x >= Game.WIDTH * 2 || this.x <= Game.WIDTH * -2) {
+			handler.removeObject(this);
+			System.out.print("Smart is removed");
+		}
 
 	}
 

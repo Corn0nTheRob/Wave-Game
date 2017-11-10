b9c740d9b25966f5f1036b817ecc3b67e8943ba3
eliasheckman <32140650+eliasheckman@users.noreply.github.com>
2017-11-09 23:10:01 -0500

    Update EnemySweep.java

--- src/mainGame/EnemySweep.java
+++ src/mainGame/EnemySweep.java
@@ -36,7 +36,23 @@ public class EnemySweep extends GameObject {
 			handler.removeObject(this);
 			System.out.print("Sweep is removed");
 		}
+		collision();
 
+	}
+	
+	public void collision() {
+
+		for (int i = 0; i < handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+
+			if (tempObject.getId() == ID.PlayerBullet) {// tempObject is an enemy
+
+				// collision code
+				if (getBounds().intersects(tempObject.getBounds())) {// player hit an enemy
+					setX(100000);
+				}
+			}
+		}
 	}
 
 	public void render(Graphics g) {

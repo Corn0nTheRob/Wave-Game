0e82fec98532ae18cfc43bbfc70af8c41dd9a1df
eliasheckman <32140650+eliasheckman@users.noreply.github.com>
2017-11-09 23:09:46 -0500

    Update EnemySmart.java

--- src/mainGame/EnemySmart.java
+++ src/mainGame/EnemySmart.java
@@ -78,7 +78,23 @@ public class EnemySmart extends GameObject {
 			handler.removeObject(this);
 			System.out.print("Smart is removed");
 		}
+		collision();
 
+	}
+	
+	public void collision() {
+
+		for (int i = 0; i < handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+
+			if (tempObject.getId() == ID.PlayerBullet) {// tempObject is an enemy
+
+				// collision code
+				if (getBounds().intersects(tempObject.getBounds())) {// player hit an enemy
+					setX(100000);
+				}
+			}
+		}
 	}
 
 	public void render(Graphics g) {
